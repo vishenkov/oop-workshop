0712fa577c9688604af30749763faa27fad0961e
Kirill Vishenkov <vishkir1991@gmail.com>
2019-02-16 09:32:59 +0300

    fix(render) should return value

--- src/render.js
+++ src/render.js
@@ -1,7 +1,6 @@
 export default (data) => {
   if (data.status === 'success') {
-    console.log(`It is ${data.country}, city ${data.city}, timezone: "${data.timezone}"`);
-  } else {
-    console.log('Request failed!');
+    return `It is ${data.country}, city ${data.city}, timezone: "${data.timezone}"`;
   }
+  return 'Request failed!';
 };
