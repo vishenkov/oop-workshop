dc0e8893e3dd264c2e0a3cae7bef65a2aaddf2ee
Kirill Vishenkov <vishkir1991@gmail.com>
2019-02-16 15:51:48 +0300

    fix(cli) add missing url param

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,8 @@
 import Geo from '.';
 import defaultRender from './render';
 
-export default async (ip, { lib, render = defaultRender } = {}) => {
-  const geo = new Geo({ lib });
+export default async (ip, { lib, baseUrl, render = defaultRender } = {}) => {
+  const geo = new Geo({ lib, baseUrl });
   const address = await geo.getInfoByIp(ip);
   return render(address);
 };
