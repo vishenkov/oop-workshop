772920bf23ead54bb897fd2410bfd42dbdd7bf17
Kirill Vishenkov <vishkir1991@gmail.com>
2019-02-16 16:24:42 +0300

    feat(pipeline) add fluent version

--- src/pipeline.js
+++ src/pipeline.js
@@ -14,3 +14,28 @@ const result = _.reduce(
 );
 
 console.log(result);
+
+
+class Pipeline {
+  constructor(pipes = []) {
+    this.pipes = pipes;
+  }
+
+  pipe(fn) {
+    return new Pipeline([...this.pipes, fn]);
+  }
+
+  exec(data) {
+    return _.reduce(this.pipes, (acc, fn) => fn(acc), data);
+  }
+}
+
+const pipeline = new Pipeline()
+  .pipe(files => _.reject(files, file => _.startsWith(file, '.')))
+  .pipe(files => _.sortBy(files))
+  .pipe(files => files[Math.round(_.size(files) / 2)])
+  .pipe(file => (_.endsWith(file, 's') ? file : `${file}s`))
+  .pipe(file => _.toUpper(file))
+  .exec(fs.readdirSync('./'));
+
+console.log(pipeline);
