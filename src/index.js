76ba6f1a9a817f0ecb4e1524afa30ce634b8c938
Kirill Vishenkov <vishkir1991@gmail.com>
2019-02-16 09:33:26 +0300

    fix(index) use correct argument

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 import getGrabber from './grabber';
 import render from './render';
 
-export default async (param = '') => {
+export default async (ip = '') => {
   const grab = getGrabber('http://ip-api.com/json', axios);
-  const response = await grab(param);
+  const response = await grab(ip);
   return render(response);
 };
