9d30650fcfe4a1b15c763ea5c6189b3611687d11
Kirill Vishenkov <vishkir1991@gmail.com>
2019-02-16 09:48:49 +0300

    refactor(index) get lib and baseUrl from params

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-import axios from 'axios';
 import getGrabber from './grabber';
 import render from './render';
 
-export default async (ip = '') => {
-  const grab = getGrabber('http://ip-api.com/json', axios);
+export default async (ip = '', { baseUrl = 'http://ip-api.com/json', lib } = {}) => {
+  const grab = getGrabber(baseUrl, lib);
   const response = await grab(ip);
   return render(response);
 };
